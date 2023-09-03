01c69afcd8bac809375fa326fd59ad3403164f25
soulcell <arsen332211@gmail.com>
2023-09-03 16:46:00 +0300

    removed hardcoded db connection string

--- Synthons.Infrastructure/SynthonsDbContext.cs
+++ Synthons.Infrastructure/SynthonsDbContext.cs
@@ -38,10 +38,6 @@ public partial class SynthonsDbContext : DbContext
 
     public virtual DbSet<ViServiceSoldTotal> ViServiceSoldTotals { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Initial Catalog=SYNTHONS;Integrated Security=True;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Customer>(entity =>
